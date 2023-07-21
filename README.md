5433d01eaab109eb1c862b92106c7e1947cb2daa
Sharareh Golshani <sharareh@Sharareh-iMac.local>
2023-07-20 22:02:00 -0700

    made changes to js to clean extra code and add comments

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # Coolest-PW-Generator
+## Description
+
+The goal for this project was to work with the given code for a company to create a password generator for their employees. To make sure the employees are given secure passwords, they'll have the choice to add and select certain criteria such as adding special characters to their password.  They'll be able to use this on their desktop browser as well as other devices such as mobile.  
+
+## Table of Contents 
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+There is no installation required for this project. Please use the link in the usage section (below) to view the password generator and generate a password for yourself. 
+
+## Usage
+
+
+https://sharareh18.github.io/Coolest-PW-Generator/
+
+
+## Credits
+
+Online Tutorials and Resources:
+
+-  JavaScript String - charAt() Method: https://www.tutorialspoint.com/javascript/string_charat.htm#:~:text=Description,%2C%20called%20stringName%2C%20is%20stringName.
+   To learn whether I needed to use charAt for getting a random password.
+
+-  Creating Javascript Random Numbers with Math.random():  https://www.udacity.com/blog/2021/04/javascript-random-numbers.html#:~:text=Javascript%20creates%20pseudo%2Drandom%20numbers,it%20will%20never%20be%201.
+   Trying to learn how to write the code for generating a random password from the array of choices.
+
+-  How to select a random element from array in JavaScript?:  https://www.geeksforgeeks.org/how-to-select-a-random-element-from-array-in-javascript/amp/
+
+-  How to Insert an Element into an Array in JS:  https://www.freecodecamp.org/news/how-to-insert-an-element-into-an-array-in-javascript/#:~:text=If%20you%20need%20to%20add,use%20the%20concat()%20method.
+   Used this to figure out how to get the chosen elements into the object array for choices. 
+
+-  Professional README Guide by edX Boot Camps LLC:  https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide
+   I copied and used the template they provided for my README, eliminating parts that did not apply.
+  
+
+University of California Irvine Full Stack Development Bootcamp 2023 Resources:
+
+-  UCI Bootcamp Slack Study Group:  https://ucivirtfsfpt0-bgx3057.slack.com/archives/C056LT7DUUF
+   Discussed push, concat and other methods to add elements to an array.  Discussed how to 
+   start by commenting the tasks into the code to follow. 
+-  UCI Web Development office hours:  Instructor Logan Garland and TA David Cox
+-  UCI Web Development Student Activities: Module 3 (mini project & student activities), Module 4 (student activities).
+   
+   
+    
+## License
+
+MIT
+
+
--- assets/script.js
+++ assets/script.js
@@ -10,70 +10,43 @@ var lowercaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n",
 "r","s","t","u","v","w","x","y","z"];
 var uppercaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q",
 "R","S","T","U","V","W","X","Y","Z"];
-
-//Variable for all user choices
 var allChoices ="";
 
-/*Function to request and confirm password criteria and generate it.  User will be asked a series of questions. 
-  First example if the chosen password length is not between 8 and 128, then user gets an alert with 
-  "you must enter a number between 8 and 128". If user selects "ok", prompt new window requesting a number again.*/
+/*Function to request and confirm password criteria and generate it.*/
 function generatePassword() {
   let lengthChoice = window.prompt("Please enter a number between 8 and 128 for your password length.");
 
     if (lengthChoice < 8 || lengthChoice > 128) {
-      alert("You must enter a number between 8 and 128.");
-      window.prompt("Please enter a number between 8 and 128.");
-    }
-
-    else {
-    /*if user doesn't confirm by clicking "ok" and clicks "cancel" instead, alert user that a number must be included
-    and if they click ok, then display new prompt for them to confirm. If confirmed, then a number from the array
-    will be added to "allChoices"*/
-    let numbersChoice = confirm("For security, your password will include numbers, ok? Please confirm.");
-    if (!numbersChoice) {
-      alert("a number MUST be included, please confirm.");
-      window.prompt("A number will be included in your password, ok?");
-    }
-    else if (numbersChoice) {
+      alert("You must enter a number between 8 and 128.")
+      window.prompt("Please enter a number between 8 and 128.")
+  } else {
+    /*If confirmed, then a number from the array will be added to "allChoices"*/
+    let numbersChoice = confirm("Would you like your password to include numbers?");
+    if (numbersChoice) {
       allChoices += numbers;
-    }
-
-
-    let lowercaseChoice = confirm("Your password will include lowercase letters, ok? Please confirm.");
-    if (!lowercaseChoice) {
-      alert("Lowercase letters MUST be included, please confirm.");
-      window.prompt("Lowercase letters will be included in your password, ok?");
-    }
-    else if (lowercaseChoice) {
-     allChoices += lowercaseLetters;
-    }
-
-
-    let uppercaseChoice = confirm("Your password will also include uppercase letters, ok? Please confirm.");
-    if (!uppercaseChoice) {
-      alert("Uppercase letters MUST be included, please confirm.");
-      window.prompt("Uppercase letters will be included in your password, ok?");
-    }
-    else if (uppercaseChoice) {
-      allChoices += uppercaseLetters;
-    } 
-      
-
-    let specialChoice = confirm("For even more security, your password will include special characters, ok? Please confirm.");
-    if (!specialChoice) {
-      alert("Special characters MUST be included, please confirm.");
-      window.prompt("Special characters will be included in your password, ok?");
-    }
-    else if (specialChoice) {
-      allChoices += specialCharacters;
-    }
   }
-    //generate new password by choosing the characters for the index randomly from the allChoices array to be the length
+
+    let lowercaseChoice = confirm("How about lowercase letters?");
+    if (lowercaseChoice) {
+     allChoices += lowercaseLetters;
+  }
+
+    let uppercaseChoice = confirm("And uppercase letters?");
+    if (uppercaseChoice) {
+      allChoices += uppercaseLetters;
+  } 
+      
+  let specialChoice = confirm("Would you also like your password to include special characters?");
+    if (specialChoice) {
+      allChoices += specialCharacters;
+  }
+  }
+        //generate new password by choosing the characters for the index randomly from the allChoices array to be the length
     //the length the user defined. 
-    let newPassword = "";
-    for (var i = 0; i < lengthChoice; i++) {
-    newPassword += allChoices.charAt(Math.floor(Math.random() * allChoices.length)); 
-    }
+  let newPassword = "";
+  for (var i = 0; i < lengthChoice; i++) {
+  newPassword += allChoices.charAt(Math.floor(Math.random() * allChoices.length)); 
+  }
   return newPassword;
 } 
 
@@ -84,6 +57,5 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-
+//attaching an eventListener to generateBtn so that writePassword is the code run when the user "clicks"
 generateBtn.addEventListener("click", writePassword);
