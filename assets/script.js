ca4c36403c57983c57e2101d2952b378bfbfeb49
Sharareh <shararehg79@yahoo.com>
2023-07-20 17:21:47 -0700

    finished creating the conditions

--- assets/script.js
+++ assets/script.js
@@ -1,65 +1,89 @@
 // Assignment Code
 
-/*the #generate is the id for the actual button*/
+
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+//Variables for password criteria defined
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]; 
+var specialCharacters = ["!","@","#","$","%","^","&","*","<",">","?"];
+var lowercaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q",
+"r","s","t","u","v","w","x","y","z"];
+var uppercaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q",
+"R","S","T","U","V","W","X","Y","Z"];
 
-/*the #password is what shows up in the text box as the pw*/
+//Variable for all user choices
+var allChoices ="";
+
+/*Function to request and confirm password criteria and generate it.  User will be asked a series of questions. 
+  First example if the chosen password length is not between 8 and 128, then user gets an alert with 
+  "you must enter a number between 8 and 128". If user selects "ok", prompt new window requesting a number again.*/
+function generatePassword() {
+  let lengthChoice = window.prompt("Please enter a number between 8 and 128 for your password length.");
+
+    if (lengthChoice < 8 || lengthChoice > 128) {
+      alert("You must enter a number between 8 and 128.");
+      window.prompt("Please enter a number between 8 and 128.");
+    }
+
+    else {
+    /*if user doesn't confirm by clicking "ok" and clicks "cancel" instead, alert user that a number must be included
+    and if they click ok, then display new prompt for them to confirm. If confirmed, then a number from the array
+    will be added to "allChoices"*/
+    let numbersChoice = confirm("For security, your password will include numbers, ok? Please confirm.");
+    if (!numbersChoice) {
+      alert("a number MUST be included, please confirm.");
+      window.prompt("A number will be included in your password, ok?");
+    }
+    else if (numbersChoice) {
+      allChoices += numbers;
+    }
+
+
+    let lowercaseChoice = confirm("Your password will include lowercase letters, ok? Please confirm.");
+    if (!lowercaseChoice) {
+      alert("Lowercase letters MUST be included, please confirm.");
+      window.prompt("Lowercase letters will be included in your password, ok?");
+    }
+    else if (lowercaseChoice) {
+     allChoices += lowercaseLetters;
+    }
+
+
+    let uppercaseChoice = confirm("Your password will also include uppercase letters, ok? Please confirm.");
+    if (!uppercaseChoice) {
+      alert("Uppercase letters MUST be included, please confirm.");
+      window.prompt("Uppercase letters will be included in your password, ok?");
+    }
+    else if (uppercaseChoice) {
+      allChoices += uppercaseLetters;
+    } 
+      
+
+    let specialChoice = confirm("For even more security, your password will include special characters, ok? Please confirm.");
+    if (!specialChoice) {
+      alert("Special characters MUST be included, please confirm.");
+      window.prompt("Special characters will be included in your password, ok?");
+    }
+    else if (specialChoice) {
+      allChoices += specialCharacters;
+    }
+  }
+    //generate new password by choosing the characters for the index randomly from the allChoices array to be the length
+    //the length the user defined. 
+    let newPassword = "";
+    for (var i = 0; i < lengthChoice; i++) {
+    newPassword += allChoices.charAt(Math.floor(Math.random() * allChoices.length)); 
+    }
+  return newPassword;
+} 
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-/*THIS IS THE LINE THAT WILL DISPLAY THE PW */
   passwordText.value = password;
 
 }
 
-/* DO I DECLARE VARS ON THE TOP?  SINCE THERE'S NO MATH, SHOULD MY NUMBERS BE STRINGS? 
-DO I PUT ALL OF THE NUMBERS OR IS 0 TO 9 ENOUGH? WILL IT DO THE MATH IF IT'S STRING?*/
 
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]; 
-var specialCharacters = ["!","@","#","$","%","^","&","*","<",">","?"];
-var lowecaseLetters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q",
-"r","s","t","u","v","w","x","y","z"];
-var uppercaseLetters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q",
-"R","S","T","U","V","W","X","Y","Z"];
-var characters = 8;
-var userChoice = ["yes"]; 
-/* IS THE USERCHOICE VAR EVEN NEEDED? THIS IS GETTING CONFUSING...COULD THE PROMPTS RETURN A SELECTION FOR THIS?
-*/
-
-function generatePassword() {
-  console.log("test");
-
-  /*a- when user clicks generate, 
-  I have to promp with "please select a number 
-  between 8-128 for your password length" 
-
-  b- when user types number between 8-128
-  then I prompt with "would you like to include lowercase, 
-  uppercase, numeric, and/or special characters?"  OR DO I HAVE TO GIVE THEM A CHOICE?
-
-  c- if yes, then I have to write a script for special char. to be included
-     if no, then the opposite ? ask about this...do I give them yes and no?
-
-
-  d- generate the password to show up in the text box  (figure out 
-    what to do with the addevent thing)*/
-
-}
-
-/*WOULD I USE "CLICK" IN PLACE OF THE QUESTION MARKS? WHAT WILL PROMPT THE MESSAGE? THE GENERATE PW BUTTON BUT 
-THAT WOULDN'T WORK HERE WOULD IT?*/
-function prompts() {
-  ??? = window.prompt("Please enter a number between 8 and 128 for your password length.");
-
-if (characters !== (characters < 8 && characters > 128)) 
-    window.alert("You must enter a number between 8 and 128.");
-
-else if (characters === (characters < 8 + characters > 128)) 
-  window.alert("Would you like to include an uppercase, lowercase, number or special character?");
-
-}
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
